d01a5e9b34b15025a9c4956e99aa2d3765b9399d
Max Huppertz <max.huppertz@gmx.net>
2018-01-29 23:49:17 -0500

    Added latest problem set

---
+++ econ_672/ps3/ps3q3.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.linalg import inv
+from os import chdir, mkdir, path
+from scipy.stats import uniform, norm
+from sklearn.neighbors import KernelDensity
+
+# PS3Q3: OLS III
+
+# Set main directory
+mdir = path.dirname(path.abspath(__file__)).replace('\\', '/')
+
+# Set figures directory
+fdir = '/figures/'
+
+# Create that directory if it doesn't exist
+if not path.isdir(mdir+fdir):
+    mkdir(mdir+fdir)
+
+# Change to figures directory
+chdir(mdir + fdir)
+
+# Set random generator seed
+np.random.seed(seed=672)
+
+# Set model parameters
+beta = np.array([.4, .9], ndmin=2).transpose()  # True betas
+N = [100, 500, 1000]  # Different sample sizes to consider
+imax = 10000  # Number of iterations per sample size
+
+Z = np.zeros(shape=(imax, len(N)))
+
+# Go through different sample sizes
+for j, n in enumerate(N):
+    # Go through all iterations for given sample size (it would be way more efficient to do this with a matrix instead
+    # of a loop, but it's not worth it optimizing this I think)
+    for i in range(imax):
+        # Generate X and U
+        X = np.column_stack((np.ones(shape=(n, 1)), uniform().rvs(size=(n, 1)) + 1))
+        U = norm(loc=0, scale=2).rvs(size=(n, 1))
+
+        # Generate y
+        y = X @ beta + U
+
+        # Calculate OLS estimate of beta_hat
+        beta_hat = inv(X.transpose() @ X) @ X.transpose() @ y
+
+        # Calculate Z_n and save it
+        Z[i, j] = beta_hat[0, 0] / beta_hat[1, 0]
+
+    # Center and divide by standard deviation
+    Z[:, j] = np.sqrt(n) * (Z[:, j] - (beta[0, 0] / beta[1, 0])) / np.std(np.sqrt(n) * Z[:, j])
+
+# Plot the distribution of Z_n for different sample sizes
+# Some plot parameters
+margin = 1  # Page margins
+figratio = 6/9  # Default ratio for plots
+
+# Set up plot of distribution
+fig, ax = plt.subplots(figsize=(8.5 - margin, (8.5 - margin) * figratio))
+
+# Grid over which to estimate kernel density (the kernel density estimator seems to like column vectors)
+x_grid = np.array(np.linspace(start=-5, stop=5, num=2000), ndmin=2).transpose()
+
+# Bandwidths for kernel density estimation (this is super hacky; it is meant to illustrate how well the approximation
+# works, but probably in large part just reflects how kernel density estimation is pretty sensitive to the bandwidth
+# selected when running it and so you can show anything using it if you don't think too hard about it... looks nice
+# though!)
+bandwidths = [.1, .6, .4]
+
+# Plot estimate of f(Z_n) for each n
+for j, (n, bw) in enumerate(zip(N, bandwidths)):
+    # Fit kernen density estimator
+    kde = KernelDensity(kernel='gaussian', bandwidth=bw).fit(np.array(Z[:, j], ndmin=2).transpose())
+
+    # Retrieve kernel density estimate for the plot
+    kde_plot = np.exp(kde.score_samples(x_grid))
+
+    # Plot the estimate
+    ax.plot(x_grid, kde_plot, linestyle='-', alpha=0.9, lw=0.9, label='$n = '+str(n)+'$')
+
+# Plot an N(0, 1) variable for comparison
+ax.plot(x_grid, norm.pdf(x_grid), linestyle='-', alpha=0.9, lw=0.9, label='$\mathcal{N}(0,1)$')
+
+# Insert a title, set axis limits, and display a legend
+fig.suptitle('Kernel density estimates of $Z_n$')
+ax.set_xlabel('$Z_n$')
+ax.set_ylabel('$f(Z_n)$', labelpad=25).set_rotation(0)
+ax.set_xlim(-3, 3)
+ax.set_ylim(-.01, 1)
+ax.legend()
+plt.show()
+
+# Save the plot
+plt.savefig('Z_density.pdf', bbox_inches='tight')
---
+++ econ_672/ps3/ps3q6.do
@@ -0,0 +1,75 @@
+qui{
+// How to clean everything
+clear *
+
+// PS3Q6: Empirical application
+// Specify directory containing Wooldridge data (this has to exist!)
+// I'm pretty sure this almost always works if you open the file directly from
+// any file management system; otherwise you'll have to hard-code the path, and
+// that's annoying
+loc dir_d = "./data"
+
+// Specify name of the data set
+loc dset = "ATTEND.dta"
+
+// Load the data
+u "`dir_d'/`dset'"
+
+// 4.14a) Regress standardized final exam score on on attendance rate and two
+// dummies for freshman (which is NOT called fresh? Why?!) and sophomore status
+
+// Specify y and X variables (this is not super beautiful code; specifically, it
+// contains lots of basically hard coded variable names, but it's only a problem
+// set, so cut me some slack, will you)
+loc y = "stndfnl"
+loc X1 = "atndrte frosh soph"
+
+// Estimate the model
+noi reg `y' `X1'
+
+// 4.14c) Add prior cumulative GPA and ACT score to the model
+
+// Add variables to the model
+loc X2 = "priGPA ACT"
+
+// Estimate the model
+noi reg `y' `X1' `X2'
+
+// 4.14e) Add squares of prior GPA and ACT score to the model
+
+// In 4.14f), I'll have to add a square of the attendance rate as well, so I'll
+// get ahead of myself here and just code that up and have STATA square it
+// together with these variables
+loc X4 = "atndrte"
+
+// Generate squared variables (for priGPA the double precision actually might
+// matter I think; without, some checks for priGPA2 != priGPA^2 come out
+// positive)
+foreach x of var `X2' `X4'{
+	gen double `x'2 = `x'^2
+	la var `x'2 "Squared version of `x'"
+	
+	// If this is one of the variables from c), add it to
+	if regexm("`X2'", "`x'"){
+		loc X3 = "`X3' `x'2"
+		}
+	else{
+		loc X4 = "`x'2"
+		}
+	}
+
+// Estimate the model
+noi reg `y' `X1' `X2' `X3'
+
+// Test for joint significance of the two squares
+noi te `X3'
+
+// 4.14f) Add a squared version of the attendance rate to the model
+
+// Estimate the model
+noi reg `y' `X1' `X2' `X3' `X4'
+
+// Test for joint significance of attendance rate and its squared value, because
+// why not?
+noi te `=word("`X1'", 1)' `X4'
+}
