30c8c88e46db4e6d00fd16a46bf9d72000a57f8e
maxhuppertz <mhupp@umich.edu>
2018-10-20 13:34:51 -0400

    still testing integration here

--- econ_641/ps1/ps1q7/econ641_ps1q7.py
+++ econ_641/ps1/ps1q7/econ641_ps1q7.py
@@ -4,7 +4,7 @@ from os import chdir, mkdir, path, mkdir
 
 # Set main directory
 mdir = path.dirname(path.abspath(__file__)).replace('\\', '/')
-#still testing
+#still testing here
 # Set data directory
 ddir = '/data'
 
