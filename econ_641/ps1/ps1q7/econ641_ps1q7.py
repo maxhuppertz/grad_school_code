835ef4f2cf586aa72a9dbd00f2b189f781f688b3
Max Huppertz <mhupp@umich.edu>
2018-10-20 12:13:25 -0400

    Update econ641_ps1q7.py

--- econ_641/ps1/ps1q7/econ641_ps1q7.py
+++ econ_641/ps1/ps1q7/econ641_ps1q7.py
@@ -1,14 +1,21 @@
-from pandas import DataFrame
+import pandas as pd
 from requests import get
 from os import path, mkdir, chdir
 
 # Set main directory
 mdir = path.dirname(path.abspath(__file__)).replace('\\', '/')
 
+# Set data directory
+ddir = '/data'
 
+data_file = ''
 
 download_data = False
+
+chdir(mdir+ddir)
 
 if download_data:
     wtiod_url = 'http://www.wiod.org/protected3/data13/wiot_analytic/'
     web_sheet = 'wiot00_row_apr12.xlsx'
+else:
+    pd.read_pickle()
