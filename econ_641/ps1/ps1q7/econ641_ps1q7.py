dc0ad0c44f56aab9cee971996b90e55975880f13
maxhuppertz <mhupp@umich.edu>
2018-10-20 13:40:06 -0400

    test

--- econ_641/ps1/ps1q7/econ641_ps1q7.py
+++ econ_641/ps1/ps1q7/econ641_ps1q7.py
@@ -4,7 +4,7 @@ from os import chdir, mkdir, path, mkdir
 
 # Set main directory
 mdir = path.dirname(path.abspath(__file__)).replace('\\', '/')
-#last test
+
 # Set data directory
 ddir = '/data'
 
@@ -17,7 +17,7 @@ data_file = 'wiot00_row_apr12'
 data_file_ext = '.xlsx'
 
 # Set download flag
-download_data = True
+download_data = False
 
 ind_icode = 'industry_code'
 ind_iname_fgood = 'industry_name_or_final_good'
@@ -40,19 +40,12 @@ if download_data:
         for chunk in web_file.iter_content(chunk_size=128):  # Go through contents on server
             local_file.write(chunk)  # Write to local file
 
-    data = pd.read_excel(data_file+data_file_ext, skiprows=[x for x in range(2)], header=[x for x in range(4)], index_col=[x for x in range(4)], skipfooter=8)
+    data = pd.read_excel(data_file+data_file_ext, skiprows=[x for x in range(2)],
+        header=[x for x in range(4)], index_col=[x for x in range(4)], skipfooter=8)
 
     data.columns.names = data_index_orig
     data.index.names = data_index_orig
 
-    data.to_pickle(data_file+'pkl')
+    data.to_pickle(data_file+'.pkl')
 else:
     data = pd.read_pickle(data_file+'.pkl')
-
-print(data.index)
-print(data.columns)
-
-#for x in [0,1]:
-#    data = data.reorder_levels(data_index_reorder, axis=x)
-
-#print(data.loc[('AUS', 'c1'), ('BEL', 'c1')])
