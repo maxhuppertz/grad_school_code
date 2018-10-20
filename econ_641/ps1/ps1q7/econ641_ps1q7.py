c2e306c30495aecc08ed0f5f075309465b21c79c
maxhuppertz <mhupp@umich.edu>
2018-10-20 15:09:11 -0400

    update

--- econ_641/ps1/ps1q7/econ641_ps1q7.py
+++ econ_641/ps1/ps1q7/econ641_ps1q7.py
@@ -19,11 +19,13 @@ data_file_ext = '.xlsx'
 # Set download flag
 download_data = False
 
+# Specify names for index levels
 ind_icode = 'industry_code'
 ind_iname_fgood = 'industry_name_or_final_good'
 ind_country = 'country'
 ind_c = 'c_num'
 
+# Make a list of the original order, as well as a reordered index
 data_index_orig = [ind_icode, ind_iname_fgood, ind_country, ind_c]
 data_index_reorder = [ind_country, ind_c, ind_iname_fgood, ind_icode]
 
@@ -32,24 +34,31 @@ chdir(mdir+ddir)
 
 # Check whether to download data
 if download_data:
+    # Specify WTIOD URL, as well as which spreadsheet to download
     wtiod_url = 'http://www.wiod.org/protected3/data13/wiot_analytic/'
     web_sheet = 'wiot00_row_apr12.xlsx'
 
+    # Access that spreadshett and save it locally
     web_file = get(wtiod_url+web_sheet, stream=True)  # Access file on server
     with open(data_file+data_file_ext, 'wb') as local_file:  # Open local file
         for chunk in web_file.iter_content(chunk_size=128):  # Go through contents on server
             local_file.write(chunk)  # Write to local file
 
+    # Read the downloaded spreadsheet into a DataFrame
     data = pd.read_excel(data_file+data_file_ext, skiprows=[x for x in range(2)],
         header=[x for x in range(4)], index_col=[x for x in range(4)], skipfooter=8)
 
+    # Specify names for index levels
     data.columns.names = data_index_orig
     data.index.names = data_index_orig
 
+    # Save the DataFrame locally
     data.to_pickle(data_file+'.pkl')
 else:
+    # Read in the locally saved DataFrame
     data = pd.read_pickle(data_file+'.pkl')
 
+# Reorder the index levels to a more usable order
 for x in range(2):
     data = data.reorder_levels(data_index_reorder, axis = x)
 
