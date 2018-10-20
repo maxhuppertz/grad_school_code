34ee2bc33d95fa3a7f0268c934f5e3d97fd47e27
Max Huppertz <mhupp@umich.edu>
2018-10-20 12:31:02 -0400

    Update econ641_ps1q7.py

--- econ_641/ps1/ps1q7/econ641_ps1q7.py
+++ econ_641/ps1/ps1q7/econ641_ps1q7.py
@@ -13,7 +13,7 @@ data_file = 'wiot00_row_apr12'
 data_file_ext = '.xlsx'
 
 # Set download flag
-download_data = False
+download_data = True
 
 data_index_orig = ['industry_code', 'industry_name_or_final_good', 'country', 'c_number']
 data_index_reorder = ['country', 'c_number', 'industry_name_or_final_good', 'industry_code']
@@ -24,7 +24,14 @@ chdir(mdir+ddir)
 # Check whether to download data
 if download_data:
     wtiod_url = 'http://www.wiod.org/protected3/data13/wiot_analytic/'
-    web_file = 'wiot00_row_apr12.xlsx'
+    web_sheet = 'wiot00_row_apr12.xlsx'
+
+    web_file = get(wtiod_url+web_sheet, stream=True)  # Access file on server
+    with open(data_file+data_file_ext, 'wb') as local_file:  # Open local file
+        for chunk in web_file.iter_content(chunk_size=128):  # Go through contents on server
+            local_file.write(chunk)  # Write to local file
+
+    data = pd.read_excel(data_file+data_file_ext, skiprows=[x for x in range(2)] header=[], skipfooter=8)
 else:
     data = pd.read_pickle(data_file+'.pkl')
 
