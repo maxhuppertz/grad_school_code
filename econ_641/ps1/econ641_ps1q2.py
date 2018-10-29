a5410f091174cbe6b369c8d75deb0539af84f4d3
maxhuppertz <mhupp@umich.edu>
2018-10-29 14:44:11 -0400

    update

--- econ_641/ps1/econ641_ps1q2.py
+++ econ_641/ps1/econ641_ps1q2.py
@@ -1,4 +1,5 @@
 # Set up model parameters
+alpha = .5
 w = 1
 beta_A = .25
 beta_B = .75
@@ -6,3 +7,10 @@ L_H = 5
 L_F = 45
 K_H = 180
 K_F = 20
+
+r = (
+    (L_H + L_F) / (K_H + K_F)
+    * ( alpha * beta_A + (1-alpha) * beta_B ) / ( alpha * (1-beta_A) + (1-alpha) * (1-beta_B) )
+    )
+
+print(r)
