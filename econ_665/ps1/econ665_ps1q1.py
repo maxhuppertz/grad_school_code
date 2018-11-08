f0176c8ee42cb9afe6f9df1ed3be203afb6c84f6
maxhuppertz <mhupp@umich.edu>
2018-11-08 00:39:39 -0500

    update

--- econ_665/ps1/econ665_ps1q1.py
+++ econ_665/ps1/econ665_ps1q1.py
@@ -2,12 +2,12 @@ import numpy as np
 
 r_min = 0
 r_max = 20
-r_step = .01
-r = np.array(np.linspace([x for x in range(r_min, r_max+1, r_step)]), ndmin=2)
+r_num = 211
+r = np.array(np.linspace(r_min, r_max+1, r_num), ndmin=2)
 
 s_min = 0
 s_max = 25
-s_step = 1
-s = np.array(np.linspace([x for x in range(s_min, s_max+1, s_step)]), ndmin=2)
+s_num = s_max + 1
+s = np.array(np.linspace(s_min, s_max, s_num), ndmin=2)
 
-beta_min_denom = r.transpose() @ s
+beta_min = ( (r.transpose() @ np.ones(r.shape)) @ np.exp( r.transpose() @ s ) ) / 1 + r.transpose() @ s
