fecf8542b391136bd24a48fb11bb61722c9f799b
maxhuppertz <mhupp@umich.edu>
2018-11-08 01:38:55 -0500

    update

--- econ_665/ps1/econ665_ps1q1.py
+++ econ_665/ps1/econ665_ps1q1.py
@@ -14,17 +14,18 @@ s = np.array(np.linspace(s_min, s_max, s_num), ndmin=2)
 beta_min = ( (r.transpose() @ np.ones(r.shape)) @ np.exp( r.transpose() @ s ) ) / ( 1 + r.transpose() @ s )
 #beta_min[np.where(beta_min > 5000)] = 2
 #beta_min[np.where(beta_min <= .1)] = 1
-print(beta_min)
+
 fig, ax = plt.subplots()
 
 heatmap = ax.imshow(beta_min, cmap='Reds', interpolation='nearest')
 
 cbar = ax.figure.colorbar(heatmap, ax=ax)
-#cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
 
-s_ticknum = 10
-ax.set_xticks(np.linspace(s_min, s_max, s_ticknum))
-ax.set_xticklabels(r)
-ax.set_yticklabels(s)
+n_ticks=10
+ax.set_xticks(np.linspace(0, beta_min.shape[0], n_ticks))
+ax.set_yticks(np.linspace(0, beta_min.shape[1], n_ticks))
+ax.set_xticklabels(np.linspace(s_min, s_max, n_ticks))
+ax.set_yticklabels(np.linspace(r_min, r_max, n_ticks))
+#cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
 
 plt.show()
