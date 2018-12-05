8e1afa3cdcdd57dfb0d762c19f5c190a49d7972e
maxhuppertz <mhupp@umich.edu>
2018-12-04 22:34:24 -0500

    update

--- econ_678/ps3/econ678_ps3q4.py
+++ econ_678/ps3/econ678_ps3q4.py
@@ -9,7 +9,7 @@ from numpy.linalg import inv
 from scipy.stats import norm
 from statsmodels.regression.linear_model import OLS as OLS_builtin
 
-# Define standard OLS regression, with Eicker-Huber-White (EHW) standard errors
+# Define standard OLS regression, with Eicker-Huber-White (EHW) variance/covariance estimator
 def OLS(y, X):
     # Get number of observations n and number of coefficients k, using X.shape[1] = k, X.shape[0] = n
     n, k = X.shape[0], X.shape[1]
@@ -18,13 +18,13 @@ def OLS(y, X):
     beta_hat = inv(X.transpose() @ X) @ (X.transpose() @ y)
 
     # Get residuals
-    U_hat = np.array(y - X @ beta_hat)
+    U_hat = y - X @ beta_hat
 
     # Calculate component of middle part of EHW sandwich
     S = X * ( U_hat @ np.ones(shape=(1,k)) )
 
-    # Calculate EHW asymptotic standard errors
-    V_hat = inv(X.transpose() @ X) @ (S.transpose() @ S) @ inv(X.transpose() @ X)
+    # Calculate EHW variance/covariance matrix
+    V_hat = ( n / (n - k) ) * inv(X.transpose() @ X) @ (S.transpose() @ S) @ inv(X.transpose() @ X)
 
     # Return coefficients and EHW variance/covariance matrix
     return beta_hat, V_hat
@@ -33,7 +33,7 @@ def OLS(y, X):
 np.random.seed(678)
 
 # Specify sample sizes
-N = [5000] #N = [10, 25, 50]
+N = [50] #N = [10, 25, 50]
 
 # Specify how often you want to run the experiment
 E = 1 #E = 1000
@@ -48,6 +48,12 @@ beta_2 = 1
 
 # Combine to (column) vector
 beta = np.array([beta_0, beta_1, beta_2], ndmin=2).transpose()
+
+# Set test level
+alpha = .05
+
+# Set up rejection counters
+reject_OLS = 0
 
 # Go through all iterations of the experiment
 for e in range(E):
@@ -66,7 +72,13 @@ for e in range(E):
 
     # Perform standard inference (using EHW standard errors)
     beta_hat_OLS, V_hat_OLS = OLS(y, X)
-    norm.ppf(.975)
 
-    check = OLS_builtin(endog=y, exog=X)
-    #print(check.fit(cov_type='HC0').cov_params())
+    CI = [beta_hat_OLS[1] - norm.ppf(1 - alpha/2) * np.sqrt(V_hat_OLS[1,1]),
+        beta_hat_OLS[1] + norm.ppf(1 - alpha/2) * np.sqrt(V_hat_OLS[1,1])]
+
+    test = OLS_builtin(y, X).fit(cov_type='HC1')
+
+    if not CI[0] <= 0 <= CI[1]:
+        reject_OLS += 1
+
+print(reject_OLS/E)
