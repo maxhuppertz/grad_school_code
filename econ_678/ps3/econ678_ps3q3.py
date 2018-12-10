3793fdf0561ea6e220cf684906bced6b78fe241e
maxhuppertz <mhupp@umich.edu>
2018-12-09 21:03:28 -0500

    update

--- econ_678/ps3/econ678_ps3q3.py
+++ econ_678/ps3/econ678_ps3q3.py
@@ -1,7 +1,25 @@
 # Import necessary modules
 import matplotlib.pyplot as plt
 import numpy as np
+from os import chdir, mkdir, path, mkdir
 from scipy.stats import norm
+
+# Set graph options
+plt.rc('font', **{'family': 'serif', 'serif': ['lmodern']})
+plt.rc('text', usetex=True)
+
+# Specify name for main directory (just uses the file's directory)
+mdir = path.dirname(path.abspath(__file__)).replace('\\', '/')
+
+# Set figures directory (doesn't need to exist)
+fdir = '/figures'
+
+# Create the figures directory if it doesn't exist
+if not path.isdir(mdir+fdir):
+    mkdir(mdir+fdir)
+
+# Change directory to figures
+chdir(mdir+fdir)
 
 # Define PDFs
 def fX(x, mu=0, sigma2=1): return norm.pdf((np.log(x) - mu)/np.sqrt(sigma2)) / x
@@ -15,10 +33,26 @@ mu = 5
 sigma2 = 1
 
 # Set up a plot
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(6.5, 4.5))
 
 # Plot PDFs
-ax.plot(x, fX(x, mu, sigma2))
-ax.plot(x, fY(x, mu, sigma2))
+ax.plot(x, fX(x, mu, sigma2), label='$f_X(x)$', color='b', linestyle='-')
+ax.plot(x, fY(x, mu, sigma2), label='$f_Y(x)$', color='g', linestyle='-.')
 
-plt.show()
+# Set axis limits
+ax.set_xlim(0, np.max(x))
+ax.set_ylim(0)
+
+# Set axis labels
+ax.set_xlabel('x', fontsize=11)
+ax.set_ylabel('Density', fontsize=11)
+
+# Enable legend
+ax.legend(fontsize=11)
+
+# Get rid of unnecessary whitespace
+fig.tight_layout()
+
+# Save figure and close
+plt.savefig('q3_densities.pdf')
+plt.close()
