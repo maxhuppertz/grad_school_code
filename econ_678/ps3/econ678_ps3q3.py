d12cf5f6cbaf27eb450051c07cea8d551d077db4
maxhuppertz <mhupp@umich.edu>
2018-12-11 09:26:00 -0500

    update

--- econ_678/ps3/econ678_ps3q3.py
+++ econ_678/ps3/econ678_ps3q3.py
@@ -23,13 +23,13 @@ chdir(mdir+fdir)
 
 # Define PDFs
 def fX(x, mu=0, sigma2=1): return norm.pdf((np.log(x) - mu)/np.sqrt(sigma2)) / x
-def fY(y, mu=0, sigma2=1): return fX(y, mu, sigma2) * (1 + np.sin(2 * np.pi * np.log(y)))
+def fY(y, mu=0, sigma2=1): return fX(y, mu, sigma2) * (1 + np.sin(2 * np.pi * (np.log(y) - mu) / sigma2))
 
 # Set up values for the horizontal axis
-x = np.linspace(10**(-10), 1000, 10000)
+x = np.linspace(10**(-10), 6, 10000)
 
 # Select parameters mu and sigma^2
-mu = 5
+mu = 0
 sigma2 = 1
 
 # Set up a plot
