513e0e9dc44d41a2fd2139dbb5b7050077651975
maxhuppertz <mhupp@umich.edu>
2018-12-23 17:57:57 +0100

    update

--- econ_641/ps2/econ641_ps2q1.py
+++ econ_641/ps2/econ641_ps2q1.py
@@ -15,41 +15,42 @@ from requests import get
 ### Part 1: Gabaix and Ibragimov (2011) estimator
 ########################################################################################################################
 
-# Set up a function which does standard OLS regression, but reports the Gabaix and Ibragimov (2011) (GI) standard error
+# Set up a function which does standard OLS regression, but reports the Gabaix and Ibragimov (2011) (GI) standard error;
+# expects to get data on log rank, minus a scaling factor, and data on log size
 def OLS_GI(rank, size, s=.5):
     # Check which rows contains entirely non-NaN values
-    use_rows = np.logical_and(np.isnan(rank), np.isnan(size))
+    use_rows = np.logical_and(np.isfinite(rank), np.isfinite(size))
 
     # Get number of observations n
     n = rank[use_rows].shape[0]
 
     # This just makes sure you can feed the function any kind of vector, or list; the try part of this statement will
-    # fail if the rank data are not a vector (i.e. not two dimensional)
-    try:
-        # Check whether this is a row vector
-        if rank.shape[0] < rank.shape[1]:
-            # If so, transpose it
-            X_1 = rank[use_rows].transpose
-        else:
-            # Otherwise, leave it as it is
-            X_1 = rank[use_rows]
-    # If the statement fails...
-    except IndexError:
-        # ...make it into a vector
-        X_1 = np.array(rank[use_rows], ndmin=2).transpose()
-    print(size.shape)
-    # Same thing, but for the size data; here, I also need to transform the data from y into y - s
+    # fail if the size data are not a vector (i.e. not two dimensional)
     try:
         # Check whether this is a row vector
         if size.shape[0] < size.shape[1]:
             # If so, transpose it
-            y = size[use_rows].transpose - .5
+            X_1 = size[use_rows].transpose
         else:
             # Otherwise, leave it as it is
-            y = size[use_rows] - .5
+            X_1 = size[use_rows]
+    # If the statement fails...
+    except IndexError:
+        # ...make it into a vector
+        X_1 = np.array(size[use_rows], ndmin=2).transpose()
+
+    # Same thing, but for the rank data; here, I also need to transform the data from y into y - s
+    try:
+        # Check whether this is a row vector
+        if rank.shape[0] < rank.shape[1]:
+            # If so, transpose it
+            y = rank[use_rows].transpose
+        else:
+            # Otherwise, leave it as it is
+            y = rank[use_rows]
     except IndexError:
         # If the first part fails, make it into a vector
-        y = np.array(size[use_rows] - .5, ndmin=2).transpose()
+        y = np.array(rank[use_rows], ndmin=2).transpose()
 
     # Set up X matrix
     X = np.concatenate((np.ones(shape=(n, 1)), X_1), axis=1)
@@ -136,9 +137,14 @@ data[v_log_sales] = np.nan
 data.loc[non_zero_sales, v_log_sales] = np.log(data.loc[non_zero_sales, v_sales])
 
 # Generate firms size rank, by year
-v_sales_rank = v_sales+'_rank'
-data[v_sales_rank] = data.groupby(v_year)[v_sales].rank()
+v_sales_rank = v_sales + '_rank'
+data[v_sales_rank] = data.groupby(v_year)[v_sales].rank() + 1
+
+# Generate log rank, with a scaling factor s, i.e. generate log(rank - s)
+v_log_sales_rank = 'log_' + v_sales_rank
+s = .5
+data[v_log_sales_rank] = np.log(data[v_sales_rank] - s)
 
 # Run OLS of log sales on log size
-beta_hat_OLS_GI, V_hat_OLS_GI = OLS_GI(data[v_sales_rank], data[v_log_sales])
+beta_hat_OLS_GI, V_hat_OLS_GI = OLS_GI(data[v_log_sales_rank], data[v_log_sales])
 print(beta_hat_OLS_GI, V_hat_OLS_GI)
