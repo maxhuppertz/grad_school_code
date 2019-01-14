75300ba2926cf9101b544758c1a714ab9c91c9c9
maxhuppertz <mhupp@umich.edu>
2019-01-14 14:24:04 -0500

    update

--- econ_632/ps1/ll_multilogit_fc.m
+++ econ_632/ps1/ll_multilogit_fc.m
@@ -1,4 +1,4 @@
-function [L,G] = ll_multilogit_fc(beta,xi,p,c)
+function [L,G,H] = ll_multilogit_fc(beta,xi,p,c)
 % Determine number of individuals and options
 [n,J] = size(p);
 
@@ -25,14 +25,39 @@ end
 % Combine the derivatives to a gradient
 G = [dbeta; dxi];
 
+% Calculate second derivative w.r.t. beta
 dbeta2 = sum( (sum((p.^2).*exp(beta*p + X),2) ...
     .* sum(exp(beta*p + X),2) - sum(p.*exp(beta*p + X),2).^2) ...
     ./ (sum(exp(beta*p + X),2).^2) );
 
+% Calculate second derivatives w.r.t. each xi_k
 dxi2(k,1) = zeros(J,1);
 for k = 1:J
     dxi2(k,1) = sum( (exp(beta*p(:,k) + X(:,k)) ...
         .* sum(exp(beta*p + X),2) - exp(beta*p(:,k) + X(:,k)).^2) ...
         ./ (sum(exp(beta*p + X),2).^2) );
 end
+
+% Set up Hessian as diagonal matrix of second derivatives
+H = diag([dbeta2; dxi2]);
+
+% Fill in cross derivatives w.r.t. to beta and xi_k or xi_k and beta
+% (these are symmetric)
+for k = 2:J+1
+    H(1,k) = sum( (p(:,k).*exp(beta*p(:,k) + X(:,k)) ...
+        .*sum(exp(beta*p + X),2) - sum(p.*exp(beta*p + X),2) ...
+        .*exp(beta*p(:,k) + X(:,k))) ...
+        ./ (sum(exp(beta*p + X),2).^2) );
+    H(k,1) = H(1,k);
+end
+
+% Fill in cross derivatives w.r.t. xi_j and xi_k (also symmetric)
+for j = 2:J+1
+    for k = 2:J+1
+        H(j,k) = -sum( (exp(beta*p(:,j) + X(:,j)) ...
+            .* exp(beta*p(:,k) + X(:,k))) ./ (sum(exp(beta*p + X),2).^2) );
+        H(k,j) = H(j,k);
+    end
+end
+
 end
