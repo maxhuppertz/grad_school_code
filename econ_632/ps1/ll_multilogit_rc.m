e6f6d399ec488b171a5b7b8ace4077674a314188
maxhuppertz <mhupp@umich.edu>
2019-01-16 13:50:52 -0500

    update

--- econ_632/ps1/ll_multilogit_rc.m
+++ econ_632/ps1/ll_multilogit_rc.m
@@ -12,7 +12,8 @@ X = ones(n,1) * xi;
 % is needed for all methods of integration used in this MLE
 function I = wnormpdf(b,beta_bar,sigma2)
     % If b is provided as a scalar, make it into a vector
-    if size(b) == [1,1]
+    % The comment beside the next line suppresses an unnecessary warning
+    if size(b) == [1,1] %#ok<BDSCA>
         b = b*ones(n,1);
     end
     
@@ -26,7 +27,7 @@ function I = wnormpdf(b,beta_bar,sigma2)
 end
 
 % Calculate log-likelihood
-if method == 'integral'
+if strcmp(method,'integral')
     % Calculate log-likelihood
     L = -sum(log(integral(@(b)wnormpdf(b,beta_bar,sigma2),-Inf,Inf, ...
         'ArrayValued',true,'RelTol',0,'AbsTol',1e-14)));
@@ -35,19 +36,19 @@ if method == 'integral'
     if L == Inf
         L = 0;
     end
-elseif method == 'monte_carlo'
+elseif strcmp(method,'monte_carlo')
     % Draw a matrix of i.i.d. N(beta_bar, sigma2) random variables
-    L = randn(n,5000) * sqrt(sigma2) + beta_bar;
+    L = randn(n,500) * sqrt(sigma2) + beta_bar;
     
-    % Convert to a cell array, where each cell contains an n x 1 vector
-    % of i.i.d. draws
-    L = num2cell(L,1);
+    % Apply the weighted normal PDF function to each column of the random
+    % numbers, which will return a 1 x 500 cell array where each cell
+    % contains an n x 1 vector of weighted normal PDF values
+    L = arrayfun(@(i)wnormpdf(L(:,i),beta_bar,sigma2),(1:500), ...
+        'UniformOutput',false);
     
-    % Calculate the weighted normal PDF for all vectors
-    L = cellfun(@(b)wnormpdf(b,beta_bar,sigma2),L,'UniformOutput',false);
-    
-    % Convert the vectors into a n x 500 matrix, take the mean within rows
-    % (this gives the choice probabilities), then take the log and sum up
+    % Convert the cell array into an n x 500 matrix using cell2mat, take
+    % the mean within rows (this gives the choice probabilities), then take
+    % the log and sum up
     L = -sum(log(mean(cell2mat(L),2)));
 end
 end
