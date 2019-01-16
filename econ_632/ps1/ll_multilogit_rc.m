181600a7366934efd4d616f4b488eeb92017be82
maxhuppertz <mhupp@umich.edu>
2019-01-16 13:37:20 -0500

    update

--- econ_632/ps1/ll_multilogit_rc.m
+++ econ_632/ps1/ll_multilogit_rc.m
@@ -11,9 +11,17 @@ X = ones(n,1) * xi;
 % Define the weighted normal PDF this is all based on as a function, which
 % is needed for all methods of integration used in this MLE
 function I = wnormpdf(b,beta_bar,sigma2)
+    % If b is provided as a scalar, make it into a vector
+    if size(b) == [1,1]
+        b = b*ones(n,1);
+    end
+    
+    % Calculate weighted normal PDF value
     I = (1/sqrt(2*pi*sigma2)) ...
         * (exp(b.*p(cidx) + X(cidx) - ((b-beta_bar).^2)/(2*sigma2)) ...
-        ./ sum(exp(b.*p + X),2));
+        ./ sum(exp((b*ones(1,J)).*p + X),2));
+    
+    % Set NaN values to zero
     I(isnan(I)) = 0;
 end
 
@@ -28,6 +36,18 @@ if method == 'integral'
         L = 0;
     end
 elseif method == 'monte_carlo'
-    L = 1;
+    % Draw a matrix of i.i.d. N(beta_bar, sigma2) random variables
+    L = randn(n,5000) * sqrt(sigma2) + beta_bar;
+    
+    % Convert to a cell array, where each cell contains an n x 1 vector
+    % of i.i.d. draws
+    L = num2cell(L,1);
+    
+    % Calculate the weighted normal PDF for all vectors
+    L = cellfun(@(b)wnormpdf(b,beta_bar,sigma2),L,'UniformOutput',false);
+    
+    % Convert the vectors into a n x 500 matrix, take the mean within rows
+    % (this gives the choice probabilities), then take the log and sum up
+    L = -sum(log(mean(cell2mat(L),2)));
 end
 end
--- econ_632/ps1/ps1_q4.m
+++ econ_632/ps1/ps1_q4.m
@@ -48,7 +48,7 @@ options = optimset('GradObj','off','HessFcn','off','Display','off', ...
 % Get the MLE using direct integration
 [theta_hat,~,~,~,~,I] = fminunc( ...
     @(theta)ll_multilogit_rc(theta(1),theta(2),[theta(3:J+1),0],p,c, ...
-    'integral'),[beta_bar0,sigma2_beta0,xi0],options);
+    'monte_carlo'),[beta_bar0,sigma2_beta0,xi0],options);
 
 % Get analytic standard errors, based on properties of correctly specified
 % MLE (variance is the negative inverse of Fisher information, estimate
@@ -58,6 +58,7 @@ SE_a = sqrt(diag(V));
 
 % Display the results
 D = cell(J+2,3);
-D(1,:) = {'theta', 'theta_hat integral', 'SE_a'};
-D(2:J+2,:) = num2cell([[mu_beta, sigma2_beta, xi(1,1:J-1)]', theta_hat', SE_a]);
+D(1,:) = {'theta', 'theta_hat', 'SE_a'};
+D(2:J+2,:) = num2cell([[mu_beta, sigma2_beta, xi(1,1:J-1)]', ...
+    theta_hat', SE_a]);
 disp(D)
