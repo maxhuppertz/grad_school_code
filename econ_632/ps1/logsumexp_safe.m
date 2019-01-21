3fd7ad53e0dff338affcb5ca68078a1220a3486d
maxhuppertz <mhupp@umich.edu>
2019-01-21 13:09:57 -0500

    update

--- econ_632/ps1/logsumexp_safe.m
+++ econ_632/ps1/logsumexp_safe.m
@@ -1,6 +1,6 @@
 function S = logsumexp_safe(X,dim)
     % Calculates the log of the sum of the exponentiated elements of X,
-    % using the 'log-exp-sum trick' to prevent overflow and limit the error
-    % caused by underflow, along dimension dim
+    % using the 'log-exp-sum trick' to prevent underflow, along dimension
+    % dim of the input array
     S = max(X,[],dim) + log(sum(exp(X - max(X,[],dim)),dim));
 end
