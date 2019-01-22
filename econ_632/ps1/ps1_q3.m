0e1c48f2a0253b2edbbef11db2f5bf333ffb6ce0
maxhuppertz <mhupp@umich.edu>
2019-01-22 10:18:20 -0500

    update

--- econ_632/ps1/ps1_q3.m
+++ econ_632/ps1/ps1_q3.m
@@ -59,34 +59,56 @@ SE_a = sqrt(diag(V));
 % Specify number of bootstrap iterations
 B = 4999;
 
-% Set up matrix of bootstrap estimates
-T = zeros(B,J);
+% Set up matrix of bootstrap estimates for the pairs bootstrap
+Tpairs = zeros(B,J);
 
 % Go through all bootstrap iterations
-% Using a pairs bootstrap is obviously pretty inefficient, and a wild
-% bootstrap would probably work better. But I've never used that with MLE,
-% and I'm not sure whether there are any big caveats I should know about
-% when doing that, so I stuck with the simpler (albeit less efficient)
-% choice.
 for b=1:B
     % Draw bootstrap sample
     i = randi([1,n],n,1);
-    
+
     % Get prices and choices for the bootstrap sample
     pstar = p(i,:);
     cstar = c(i,:);
-    
-    % Run MLE
-    T(b,:) = fminunc( ...
+
+    % Run MLE, add the results to the matrix of bootstrap estimates
+    Tpairs(b,:) = fminunc( ...
         @(theta)ll_multilogit_fc(theta(1),[theta(2:J),0],pstar,cstar,1),...
         [beta0,xi0],options);
 end
 
-% Get the boostrapped standard errors
-SE_b = sqrt(sum((T - ones(B,1) * theta_hat).^2,1) / B);
+% Get the boostrapped standard errors for the pairs bootstrap
+SE_bpairs = sqrt(sum((Tpairs - ones(B,1) * theta_hat).^2,1) / B);
+
+% Set up matrix of bootstrap estimates for the parametric bootstrap
+Tpairs = zeros(B,J);
+
+% Go through all bootstrap iterations
+for b=1:B
+    % Draw innovations
+    epsstar = evrnd(0,1,n,J);
+
+    % Calculate utility based on original estimate and new innovations
+    ustar = ...
+        theta_hat(1,1)*p + ones(n,1)*[theta_hat(1,2:J),0] + epsstar;
+
+    % Get choices
+    [~,cstar] = max(ustar,[],2);
+
+    % Calculate MLE, add it to the matrix of bootstrap estimates
+    Tparam(b,:) = fminunc( ...
+        @(theta)ll_multilogit_fc(theta(1),[theta(2:J),0],p, ...
+        cstar,1),[theta_hat(1,1),theta_hat(1,2:J)],options);
+end
+
+% Get the boostrapped standard errors for the parametric bootstrap
+SE_bparam = sqrt(sum((Tparam - ones(B,1) * theta_hat).^2,1) / B);
 
 % Display the results
-D = cell(J+1,4);
-D(1,:) = {'theta', 'theta_hat', 'SE_a', strcat('SE_b (T=',num2str(B),')')};
-D(2:J+1,:) = num2cell([[beta, xi(1,1:J-1)]', theta_hat', SE_a, SE_b']);
+D = cell(J+1,5);
+D(1,:) = {'theta', 'theta_hat', 'SE_a', ...
+    strcat('SE_bpa (T=',num2str(B),')'), ...
+    strcat('SE_bpm (T=',num2str(B),')')};
+D(2:J+1,:) = num2cell([[beta, xi(1,1:J-1)]', theta_hat', SE_a, ...
+    SE_bpairs', SE_bparam']);
 disp(D)
