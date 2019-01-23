8777e5e08aa28a8cff68338cf62e31dbada89f39
maxhuppertz <mhupp@umich.edu>
2019-01-23 10:21:10 -0500

    update

--- econ_632/ps1/ps1_q5.m
+++ econ_632/ps1/ps1_q5.m
@@ -14,22 +14,22 @@ M = 100;
 m = ceil((1:n)' * (M/n));
 
 % Set up xi, where the [m,j] element of this vector equals xi_{mj}
-mu_xi = [1,1.5,0];  % Mean of the quality for each product
-sigma2_xi = [2,.5,2];  % Variance for each product
+mu_xi = [11,12,10];  % Mean of the quality for each product
+sigma2_xi = [1,.5,1.2];  % Variance for each product
 xi = randn(M,J) .* (ones(M,1) * sqrt(sigma2_xi)) + ones(M,1) * mu_xi;
 
-% Set up Z, where the mth element of this column vector equals Z_m
-mu_Z = 0;  % Location of base distribution for Z
-sigma2_Z = 1;  % Variance of base distribution for Z
+% Set up Z, where the [m,j] element of this [M,J] matrix equals Z_{mj}
+mu_Z = 0;  % Mean Z
+sigma2_Z = 2;  % Variance of Z
 
-% Draw Z as lognormal(mu_Z,sigma_Z) random variable
+% Draw Z as N(mu_Z,sigma2_Z) random variable
 Z = randn(M,J)*sqrt(sigma2_Z) + mu_Z;
 
 % Set coefficient for pricing equation (how the instrument affects price)
-gamma_Z = 2;
+gamma_Z = 1;
 
-% Get prices as quality plus Z times price coefficient
-sigma2_p = 1;
+% Get prices, which have an additional disturbance built in
+sigma2_p = .2;
 p = xi + gamma_Z*Z + randn(M,J)*sqrt(sigma2_p);
 
 % Draw epsilon as Gumbel(0,1) i.i.d. random variables
@@ -60,11 +60,14 @@ lnS = log(S);
 % Markets with zero share for any good will cause a problem, since they
 % will cause the IV estimation to return NaNs. Select whether to add a tiny
 % amount to the share of the good in question for that market
-addtozeros = 0;
+addtozeros = 1;
 
 % Add to the shares of the goods in question, if desired
 if addtozeros
     lnS(lnS==-Inf) = 10^(-14);
+    
+    % Display a message that this happened
+    fprintf('\nZero shares set to 10^(-14)\n')
 end
 
 % Get IV sample (markets with non-zero shares for all goods)
@@ -77,57 +80,30 @@ Mivsamp = sum(ivsamp);
 fprintf(['\nEffective number of markets: ',num2str(Mivsamp),'\n'])
 
 % Set up cell array to display results
-D = cell(J+1,4);
+D = cell(3,4);
 D(1,:) = {'Parameter', 'True value', 'Estimate', 'SE_a'};
-D(J+1,1) = {'beta'};
+D(2,1) = {'Intercept'};
+D(2,2) = {'n/a'};
+D(3,1) = {'beta'};
+D(3,2) = num2cell(beta);
 
 % Created vector of log share differences (option J is the outside good)
 DlnSflat = ...
     reshape(lnS(ivsamp,1:J-1),[Mivsamp*(J-1),1]) ...
     - reshape(lnS(ivsamp,J)*ones(1,J-1),[Mivsamp*(J-1),1]);
 
-% Create dummies for all options other than J
-Dxi = zeros((J-1)*Mivsamp,J-2);  % Matrix of dummies
-
-% Go through all such options
-for j=1:J-1
-    % Check whether this is the first such option
-    if j == 1
-        % If so, just make this an intercept
-        d = ones(J-1,1);
-        
-        % Add a label to the results matrix
-        D(j+1,1) = cellstr(strcat( ...
-            'mu_xi_',num2str(j),' -',' mu_xi_',num2str(J)));
-    else
-        % Otherwise, start with a vector of zeros
-        d = zeros(J-1,1);
-
-        % Make the jth element equal to one
-        d(j) = 1;
-        
-        % Add a label to the results matrix
-        D(j+1,1) = cellstr(strcat('mu_xi_',num2str(j),' - mu_xi_1'));
-    end
-    
-    % Put it in the matrix
-    Dxi(:,j) = repmat(d,Mivsamp,1);
-end
-
 % Create flattened version of price and instrument data
 pflat = reshape(p(ivsamp,1:J-1),[Mivsamp*(J-1),1]);
 Zflat = reshape(Z(ivsamp,1:J-1),[Mivsamp*(J-1),1]);
 
-% Run 2SLS on the flattened (pooled) data
-[theta_hat,Sigma_hat] = ivreg(DlnSflat, [Dxi,pflat], ...
-    [Dxi,Zflat]);
+% Run 2SLS on the flattened (pooled) data, including an intercept
+[theta_hat,Sigma_hat] = ivreg(DlnSflat, [ones((J-1)*Mivsamp,1),pflat], ...
+    [ones((J-1)*Mivsamp,1),Zflat]);
 
 % Calculate standard errors
 SE_a = sqrt(diag(Sigma_hat));
 
 % Display the results
-D(2:J+1,2:4) = num2cell([ ...
-    [mu_xi(1,1)-mu_xi(1,J), mu_xi(1,2:J-1)-mu_xi(1,J), beta]', ...
-    theta_hat, SE_a]);
+D(2:3,3:4) = num2cell([theta_hat, SE_a]);
 fprintf('\n2SLS estimates\n')
 disp(D)
