909b5dad4ee208d644592b7ce9d3fbfe9cbc6f91
maxhuppertz <mhupp@umich.edu>
2019-01-24 21:40:00 -0500

    update

--- econ_666/ps1/econ666_ps1q3.py
+++ econ_666/ps1/econ666_ps1q3.py
@@ -7,16 +7,16 @@ mdir = path.dirname(path.abspath(__file__)).replace('\\', '/')
 
 # Change directory
 chdir(mdir)
+n = 100000
+X = np.random.normal(size=(n,1))
+y = .5*X + np.random.normal(size=(n,1)) * 5
 
-X = np.random.normal(size=(10000,1))
-y = .5*X + np.random.normal(size=(10000,1)) * 5
-
-[beta_hat,Sigma_hat] = ols(y,np.concatenate((np.ones(shape=(10000,1)),X),axis=1))
+[beta_hat,Sigma_hat] = ols(y,np.concatenate((np.ones(shape=(n,1)),X),axis=1))
 print(beta_hat.flatten())
 print(np.sqrt(np.diag(Sigma_hat)))
 print()
 
-[beta_hat,Sigma_hat] = ols(y,np.concatenate((np.ones(shape=(10000,1)),X),axis=1),cov_est='hc1')
+[beta_hat,Sigma_hat] = ols(y,np.concatenate((np.ones(shape=(n,1)),X),axis=1),cov_est='hc1')
 print(beta_hat.flatten())
 print(np.sqrt(np.diag(Sigma_hat)))
 print()
