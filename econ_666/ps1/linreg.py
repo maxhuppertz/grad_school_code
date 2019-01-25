0627f0248bd55941b858bab7b9ec2cf59bac3ace
maxhuppertz <mhupp@umich.edu>
2019-01-24 19:52:27 -0500

    update

--- econ_666/ps1/linreg.py
+++ econ_666/ps1/linreg.py
@@ -1,9 +1,9 @@
+# Import necessary packages
+import numpy as np
+from numpy.linalg import solve
+
 # This function just runs a standard linear regression of y on X
 def ols(y, X, get_cov=True, cov_est='homoskedastic'):
-    # Import necessary packages
-    import numpy as np
-    from numpy.linalg import solve
-
     # Get number of observations n and number of coefficients k
     n, k = X.shape[0], X.shape[1]
 
@@ -19,7 +19,7 @@ def ols(y, X, get_cov=True, cov_est='homoskedastic'):
         # Check which covariance estimator to use
         if cov_est == 'homoskedastic':
             # For the homoskedastic estimator, just calculate the standard variance
-            V_hat = ( n / (n - k) ) * XXinv * (U_hat.transpose() @ U_hat)
+            V_hat = ( 1 / (n - k) ) * XXinv * (U_hat.transpose() @ U_hat)
         elif cov_est == 'hc1':
             # Calculate component of middle part of EHW sandwich (S_i = X_i u_i, meaning that it's easy to calculate
             # sum_i X_i X_i' u_i^2 = S'S)
