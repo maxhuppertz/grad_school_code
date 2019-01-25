8bf7c5e185e61f93945fa53ede2825e24003fe41
maxhuppertz <mhupp@umich.edu>
2019-01-24 22:23:29 -0500

    update

--- econ_666/ps1/econ666_ps1q3.py
+++ econ_666/ps1/econ666_ps1q3.py
@@ -12,6 +12,12 @@ chdir(mdir)
 # Specify pairs of correlations
 corrs = [[0,0], [.1,.1], [.6,.1], [.1,.6]]
 
+# Set sample sizes
+sampsi = [10, 25, 100]
+
+# Set treatment probabilities
+tprobs = [.3, .5]
+
 # Specify number of tuples
 T = 100
 
@@ -19,11 +25,14 @@ for corr in corrs:
     # Set up covariance matrix
     C = np.eye(len(corr)+1)
 
+    # Fill in off-diagonal elements
     for i, c in enumerate(corr):
         C[0,i+1] = C[i+1,0] = c
 
+    # Get data
     D = np.random.multivariate_normal(np.zeros(len(corr)+1), C, size=T)
 
+    # Split them up into X, y, and tau
     X = D[:,0]
     y = D[:,1]
     tau = D[:,2:]
