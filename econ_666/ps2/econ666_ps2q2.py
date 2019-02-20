f160e459487411f9edc6d79d7b7a748e65ad8cf3
maxhuppertz <mxhupp@gmail.com>
2019-02-20 09:52:12 -0500

    update

--- econ_666/ps2/econ666_ps2q2.py
+++ econ_666/ps2/econ666_ps2q2.py
@@ -6,16 +6,19 @@
 import matplotlib as mpl
 
 # Select backend that does not open figures interactively (has to be done before
-# pyplot is imported)
+# pyplot is imported); without this, Python will get confused when it tries to
+# close figures, and it will send annoying warnings
 mpl.use('Agg')
 
 # Import other necessary packages and functions
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from inspect import getsourcefile
-import matplotlib.pyplot as plt
+from joblib import Parallel, delayed
+from multiprocessing import cpu_count
 from os import chdir, mkdir, path
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, minimize
 from scipy.stats import norm
 from scipy.stats import t
 
@@ -38,9 +41,9 @@ from linreg import ols
 ################################################################################
 
 # Set graph options
-mpl.rcParams["text.latex.preamble"].append(r'\usepackage{amsmath}')
-plt.rc('font', **{'family': 'serif', 'serif': ['lmodern']})
-plt.rc('text', usetex=True)
+#mpl.rcParams["text.latex.preamble"].append(r'\usepackage{amsmath}')
+#plt.rc('font', **{'family': 'serif', 'serif': ['lmodern']})
+#plt.rc('text', usetex=True)
 
 # Set figures/tables directory (doesn't need to exist)
 fdir = '/figures'
@@ -166,20 +169,40 @@ print('\n2(d) Sample size needed:', N_q2d)
 ################################################################################
 
 # Define a function to calculate the MDE
-def MDE(N, alpha, kappa, p, sigma2):
+def MDE(N, alpha, kappa, p, sigma2, F_crit=None):
     # Define the equation defining the MDE
     def MDE_eq(E):
-        # Distribution for the control group
-        #Fc = norm()
-        Fc = t(df=p*N)
+        # Check whether a distribution of test statistics was supplied as a
+        # Numpy column vector; see question 2(h) for why that is useful
+        if F_crit is None:
+            # Distribution for the control group
+            Fc = t(df=p*N)
 
-        # Distribution for the treatment group
-        Ft = t(df=p*N, loc=E)
+            # Get the critical value from that distribution
+            crit_control = Fc.ppf(1-alpha/2)
+
+            # Distribution for the treatment group
+            Ft = t(df=p*N, loc=E)
+
+            # Get critical value for the treatment group
+            crit_treatment = Ft.ppf(1-kappa)
+        else:
+            # Get index of element needed for a two-sided test at level alpha
+            alpha_idx = np.int((1-alpha/2)*F_crit.shape[0] - 1)
+
+            # Get critical value for the control group from supplied values
+            crit_control = F_crit[alpha_idx,0]
+
+            # Get index of element needed for kappa level of power
+            kappa_idx = np.int((1-kappa)*F_crit.shape[0] - 1)
+
+            # Get critical values for the treatment group
+            crit_treatment = F_crit[kappa_idx,0] + E
 
         # Calculate discrepancy
         delta_MDE = (
             E
-            - (Fc.ppf(1-alpha/2) + Ft.ppf(1-kappa))
+            - (crit_control + crit_treatment)
             * np.sqrt( (p * (1-p))**(-1) * sigma2 / N))
 
         # Return discrepancy
@@ -197,7 +220,7 @@ vec_MDE = np.vectorize(MDE)
 
 # Specify minimum and maximum number of N over which to plot
 Nmin = 500
-Nmax = 3000
+Nmax = 501
 
 # Calculate number of steps (using only integers)
 Nsteps = Nmax - Nmin + 1
@@ -260,6 +283,10 @@ plt.savefig(figname_q2e, bbox_inches='tight')
 plt.close('fig_mde_n')
 
 ################################################################################
+### 2(g)
+################################################################################
+
+################################################################################
 ### 2(h)
 ################################################################################
 
@@ -273,9 +300,90 @@ f_lg = .5
 p_sm = .7  # Small villages
 p_lg = .3  # Large villages
 
+# Calculate population in small and large villages
+N_sm = (1-f_lg) * N_q2h
+N_lg = f_lg * N_q2h
+
 # Generate outcome for the unskilled population
-Y_unskilled = np.random.normal(size=(N_q2h, 1), scale=sd_unskilled)
+y_unskilled = np.random.normal(size=(N_q2h, 1), scale=sd_unskilled)
 
+# Generate village indicator
+# Draw random normals
+V_lg = np.random.normal(size=N_q2h)
 
+# Allocate everyone ranked at or below the number of people in large villages
+# to a large village; that is, V[i] = 1 <=> i lives in a large village
+V_lg = (V_lg.argsort() + 1 <= N_lg)
+
+# Generate treatment indicator
+# Draw random normals
+W = np.random.normal(size=N_q2h)
+
+# Replace the treatment in small villages as 1 with probability p_sm
+W[~V_lg] = (W[~V_lg].argsort() + 1 <= p_sm * N_sm)
+
+# Replace the treatment in large villages as 1 with probability p_lg
+W[V_lg] = (W[V_lg].argsort() + 1 <= p_lg * N_lg)
+
+# Make the treatment indicator into a proper vector
+W = np.array(W, ndmin=2).transpose()
+
+# Generate observed data
+y = y_unskilled + beta * W
+
+# Make an intercept for regressions
+cons = np.ones(shape=(N_q2h, 1))
+
+# Specify number of simulations to get the randomization distribution
+R = 1000
+
+# Define a function to do one iteration of the randomization distribution
+def random_t(N, y, x1, V, N_0, N_1, p_0, p_1, seed):
+    # Set random number generator's seed
+    np.random.seed(seed)
+
+    # Generate treatment indicator
+    # Draw random normals
+    W = np.random.normal(size=N)
+
+    # Replace the treatment in small villages as 1 with probability p_sm
+    W[~V] = (W[~V].argsort() + 1 <= p_0 * N_0)
+
+    # Replace the treatment in large villages as 1 with probability p_lg
+    W[V] = (W[V].argsort() + 1 <= p_1 * N_1)
+
+    # Make the treatment indicator into a proper vector
+    W = np.array(W, ndmin=2).transpose()
+
+    # Generate RHS data
+    X = np.concatenate((x1, W), axis=1)
+
+    # Estimate regression, get t-statistics
+    _, _, t = ols(y, X, cov_est='hmsd', get_p=False)
+
+    # Return t-statistic for treatment dummy
+    return t[1,0]
+
+# Get number of available cores
+ncores = cpu_count()
+
+# Get randomization distribution
+T = Parallel(n_jobs=ncores)( delayed(random_t)
+        (N=N_q2h, y=y, x1=cons, V=V_lg, N_0=N_sm, N_1=N_lg, p_0=p_sm, p_1=p_lg,
+        seed = r) for r in range(R) )
+
+# Make the randomization t-statistics into a proper (column) vector
+T = np.array(T, ndmin=2).transpose()
+
+# Sort the t-statistics (Python sorts from smallest to largest)
+T.sort(axis=0)
+
+# Calculate MDE
+MDE_q2h = MDE(
+    N=N_q2h, alpha=alpha, kappa=kappa, p=p, sigma2=sd_unskilled**2, F_crit=T
+    )
+
+# Print the results
+print('\n2(h) MDE:', MDE_q2h)
 
 #
