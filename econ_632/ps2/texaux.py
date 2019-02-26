1eab79bedd5255d24f79495eaba62865c69801ff
maxhuppertz <mxhupp@gmail.com>
2019-02-26 18:38:37 -0500

    update

---
+++ econ_632/ps2/texaux.py
@@ -0,0 +1,38 @@
+# Make a simple LaTeX table (only the rows)
+def textable(X, fname='table.tex', prec=4):
+    # Create formatting string
+    fstring = '{:,.'+str(prec)+'f}'
+
+    # Determine shape of input, which is also the shape of the table
+    n, k = X.shape
+
+    # Open the file
+    with open(fname, mode='w') as file:
+        # Go through all rows
+        for i in range(n):
+            # Start with an empty string for this row
+            thisrow = ''
+
+            # Go through all columns
+            for j in range(k):
+                # Check whether the current entry is a string
+                if type(X[i, j]) != str:
+                    # If not, format it, according to the precision parameter
+                    entry = fstring.format(round(X[i, j], 4))
+                else:
+                    # If it is, just use it as-is
+                    entry = X[i, j]
+
+                # Check whether this is the last entry in this row
+                if j != k-1:
+                    # If not, add an ampersand after the entry
+                    delim = r' & '
+                else:
+                    # If it is, add a double backslash
+                    delim = r' \\'
+
+                # Add the entry and delimiter to the row
+                thisrow = thisrow + entry + delim
+
+            # Write the row to the file
+            file.write(thisrow)
